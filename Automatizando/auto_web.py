f62df0608aca2a6a76916c7eee08dbe629fdf796
Otávio Wolff Buffon <100782160+taviowolff@users.noreply.github.com>
2024-10-09 10:16:37 -0300

    20241009

--- Automatizando/auto_web.py
+++ Automatizando/auto_web.py
@@ -1,39 +1,39 @@
 # # Passo 1: entrar no sistema da empresa
 
-# import pyautogui
-# from time import sleep
+import pyautogui
+from time import sleep
 
-# # pyatogui.write -> escrever um texto
-# # pyautogui.press -> apertar 1 tecla
-# # pyautogui.click -> clicar em algum lugar da tela
-# # pyautogui.hotkey -> combinação de teclas
+# pyatogui.write -> escrever um texto
+# pyautogui.press -> apertar 1 tecla
+# pyautogui.click -> clicar em algum lugar da tela
+# pyautogui.hotkey -> combinação de teclas
 
-# pyautogui.PAUSE = 0.1 # Definição de tempo entre códigos
+pyautogui.PAUSE = 0.1 # Definição de tempo entre códigos
 
-# # abrir o navegador (chrome)
-# pyautogui.press("win")
-# pyautogui.write("chrome")
-# pyautogui.press("enter")
-# sleep(1)
-# pyautogui.press("tab")
-# pyautogui.press("enter")
+# abrir o navegador (chrome)
+pyautogui.press("win")
+pyautogui.write("chrome")
+pyautogui.press("enter")
+sleep(1)
+pyautogui.press("tab")
+pyautogui.press("enter")
 
-# # entrar no link
-# pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
-# pyautogui.press("enter")
-# sleep(3)
+# entrar no link
+pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+pyautogui.press("enter")
+sleep(3)
 
-# # Passo 2: Fazer login
-# # Selecionar o campo de email
-# pyautogui.click(x=1100,y=408)
-# # Escrever o seu email
-# pyautogui.write("pythonimpressionador@gmail.com")
-# pyautogui.press("tab") # selecionar próximo campo
-# pyautogui.write("sua senha")
-# pyautogui.press("tab")
-# pyautogui.press("enter")
-# sleep(3)
-# pyautogui.press("enter")
+# Passo 2: Fazer login
+# Selecionar o campo de email
+pyautogui.click(x=1100,y=408)
+# Escrever o seu email
+pyautogui.write("pythonimpressionador@gmail.com")
+pyautogui.press("tab") # selecionar próximo campo
+pyautogui.write("sua senha")
+pyautogui.press("tab")
+pyautogui.press("enter")
+sleep(3)
+pyautogui.press("enter")
 
 # Passo 3: importar a base de produtos pra cadastrar
 import pandas as pd
